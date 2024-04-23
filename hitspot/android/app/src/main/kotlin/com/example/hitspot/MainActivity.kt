5bc5c26acdb504f4b1acb2840f1b2bd9713768e3
Wojciech Trapkowski <wojciechtrapkowski1@gmail.com>
2024-04-23 15:18:37 +0200

    [HitSpot][Backend] Better setup
    1. Fixed Android build
    2. Added README
    3. Added VSCode launch presets

--- hitspot/android/app/src/main/kotlin/com/example/hitspot/MainActivity.kt
+++ hitspot/android/app/src/main/kotlin/com/example/hitspot/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.hitspot
+package com.company.hitspot
 
 import io.flutter.embedding.android.FlutterActivity
 
--- hitspot/lib/main.dart
+++ hitspot/lib/main.dart
@@ -2,11 +2,12 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_config/flutter_config.dart';
 import 'package:get/get.dart';
+import 'package:hitspot/firebase_options.dart';
 import 'package:hitspot/utils/hs_app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   await FlutterConfig.loadEnvVariables();
   Get.put(HSApp());
   runApp(const MyApp());
