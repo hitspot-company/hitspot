7a803e16da314db369d950341c315e6327aa2c1b
Jakub Nenczak <kubanenczak@gmail.com>
2024-06-20 18:49:47 +0200

    Environment setup

--- hitspot-supabase/hitspot/lib/features/login/magic_link/view/magic_link_sent_page.dart
+++ hitspot-supabase/hitspot/lib/features/login/magic_link/view/magic_link_sent_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:gap/gap.dart';
 import 'package:hitspot/constants/constants.dart';
 import 'package:hitspot/features/login/magic_link/cubit/hs_magic_link_cubit.dart';
+import 'package:hitspot/widgets/form/hs_form.dart';
 import 'package:hitspot/widgets/hs_appbar.dart';
 import 'package:hitspot/widgets/hs_button.dart';
 import 'package:hitspot/widgets/hs_loading_indicator.dart';
@@ -70,11 +71,10 @@ class MagicLinkSentPage extends StatelessWidget {
             child: BlocSelector<HSMagicLinkCubit, HSMagicLinkState, bool>(
               selector: (state) => state.status == HSMagicLinkStatus.verifying,
               builder: (context, isVerifying) {
-                return HSButton(
-                  onPressed: isVerifying ? null : magiclinkCubit.verifyOtp,
-                  child: isVerifying
-                      ? const HSLoadingIndicator()
-                      : const Text("Verify"),
+                if (isVerifying) return HSFormButton.loading();
+                return HSFormButton(
+                  onPressed: magiclinkCubit.verifyOtp,
+                  child: const Text("Verify"),
                 );
               },
             ),
