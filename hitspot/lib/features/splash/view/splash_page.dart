5bc48007b810e31016d1c6204dc222d72d09bf43
Jakub Nenczak <kubanenczak@gmail.com>
2024-06-26 19:23:25 +0200

    Splash working

--- hitspot/lib/features/splash/view/splash_page.dart
+++ hitspot/lib/features/splash/view/splash_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:hitspot/constants/constants.dart';
+import 'package:hitspot/widgets/hs_scaffold.dart';
+import 'package:lottie/lottie.dart';
 
 class SplashPage extends StatefulWidget {
   const SplashPage({super.key});
@@ -8,17 +11,42 @@ class SplashPage extends StatefulWidget {
   State<SplashPage> createState() => _SplashPageState();
 }
 
-class _SplashPageState extends State<SplashPage> {
+class _SplashPageState extends State<SplashPage> with TickerProviderStateMixin {
+  late final AnimationController _controller;
+  final String animation = "assets/splash/splash_right.json";
+
   @override
   void initState() {
     super.initState();
-    app.authenticationBloc.initializeStreamSubscription();
+    _controller = AnimationController(vsync: this);
+    _controller.addListener(_listenAndNavigate);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: CircularProgressIndicator()),
+    return HSScaffold(
+      backgroundColor: appTheme.mainColor,
+      body: Lottie.asset(
+        controller: _controller,
+        frameRate: const FrameRate(60),
+        onLoaded: (composition) {
+          _controller.duration = composition.duration;
+          _controller.forward();
+        },
+        animation,
+      ),
     );
+  }
+
+  void _listenAndNavigate() {
+    if (_controller.isCompleted) {
+      app.authenticationBloc.initializeStreamSubscription();
+    }
   }
 }
