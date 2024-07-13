c60db89c808c77861e5d0a2b7cc48aba32ef1172
Wojciech Trapkowski <wojciechtrapkowski1@gmail.com>
2024-07-13 18:38:08 +0200

    [WT][Frontend] Improve refresh indicator design (#33)
    Co-authored-by: Wojtek Trapkowski <wojtektrapkowski@MacBook-Pro-Wojtek.local>

--- hitspot/lib/features/home/main/view/home_page.dart
+++ hitspot/lib/features/home/main/view/home_page.dart
@@ -5,6 +5,7 @@ import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:gap/gap.dart';
 import 'package:hitspot/constants/constants.dart';
 import 'package:hitspot/extensions/hs_sliver_extensions.dart';
+import 'package:hitspot/features/app/hs_app.dart';
 import 'package:hitspot/features/home/main/cubit/hs_home_cubit.dart';
 import 'package:hitspot/utils/theme/hs_theme.dart';
 import 'package:hitspot/widgets/hs_image.dart';
@@ -28,8 +29,11 @@ class HomePage extends StatelessWidget {
         builder: (context, status) {
           final isLoading = status == HSHomeStatus.loading;
           final trendingBoards = homeCubit.state.tredingBoards;
-          return RefreshIndicator.adaptive(
+          return RefreshIndicator(
             onRefresh: homeCubit.handleRefresh,
+            color: HSTheme.instance.mainColor,
+            backgroundColor: Colors.white,
+            strokeWidth: 3.0,
             child: CustomScrollView(
               slivers: [
                 SliverAppBar(
