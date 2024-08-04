ad7674d1c026d238ead8e01d527a9673d57d6891
Jakub Nenczak <kubanenczak@gmail.com>
2024-08-04 18:55:04 +0200

    Launches

--- hitspot/lib/main.dart
+++ hitspot/lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_config/flutter_config.dart';
 import 'package:hitspot/constants/constants.dart';
 import 'package:hitspot/features/authentication/hs_authentication_bloc.dart';
 import 'package:hitspot/features/connectivity/bloc/hs_connectivity_bloc.dart';
@@ -14,12 +13,13 @@ import 'package:hs_location_repository/hs_location_repository.dart';
 import 'package:hs_storage_repository/hs_storage_repository.dart';
 import 'package:hs_theme_repository/hs_theme.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await FlutterConfig.loadEnvVariables();
-  final url = FlutterConfig.get("SUPABASE_URL");
-  final anon = FlutterConfig.get("SUPABASE_ANON_KEY");
+  await dotenv.load(fileName: ".env");
+  final url = dotenv.env["SUPABASE_URL"]!;
+  final anon = dotenv.env["SUPABASE_ANON_KEY"]!;
   HSDebugLogger.logInfo("$url: $anon");
   await Supabase.initialize(url: url, anonKey: anon);
 
@@ -65,8 +65,8 @@ class MyApp extends StatelessWidget {
         ),
         RepositoryProvider(
           create: (_) => HSLocationRepository(
-            FlutterConfig.get("GOOGLE_MAPS_KEY"),
-            FlutterConfig.get("GOOGLE_MAPS_KEY"),
+            dotenv.env["GOOGLE_MAPS_KEY"]!,
+            dotenv.env["GOOGLE_MAPS_KEY"]!,
           ),
         ),
       ],
