9e02cf06b6f92797be7bb710c7b10208142fd4fc
Wojciech Trapkowski <wojciechtrapkowski1@gmail.com>
2024-09-13 19:21:02 +0200

    Remove popping when on map page

--- hitspot/lib/features/boards/single/map/view/single_board_map_page.dart
+++ hitspot/lib/features/boards/single/map/view/single_board_map_page.dart
@@ -17,67 +17,70 @@ class SingleBoardMapPage extends StatelessWidget {
     final cubit = BlocProvider.of<HSSingleBoardMapCubit>(context);
     const builderSidePadding = 16.0;
     final builderWidth = screenWidth - (builderSidePadding * 2);
-    return HSScaffold(
-      sidePadding: 0.0,
-      appBar: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 8.0),
-        child: HSAppBar(
-          titleText: cubit.board.title,
-          enableDefaultBackButton: true,
-          right: IconButton(
-              onPressed: cubit.resetCamera,
-              icon: const Icon(FontAwesomeIcons.magnifyingGlassMinus)),
+    return PopScope(
+      canPop: false,
+      child: HSScaffold(
+        sidePadding: 0.0,
+        appBar: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 8.0),
+          child: HSAppBar(
+            titleText: cubit.board.title,
+            enableDefaultBackButton: true,
+            right: IconButton(
+                onPressed: cubit.resetCamera,
+                icon: const Icon(FontAwesomeIcons.magnifyingGlassMinus)),
+          ),
         ),
-      ),
-      body: BlocSelector<HSSingleBoardMapCubit, HSSingleBoardMapState,
-          HSSingleBoardMapStatus>(
-        selector: (state) => state.status,
-        builder: (context, status) {
-          if (status == HSSingleBoardMapStatus.loading) {
-            return const HSLoadingIndicator();
-          } else if (status == HSSingleBoardMapStatus.error) {
-            return const Center(child: Text('Error loading map'));
-          }
-          return BlocBuilder<HSSingleBoardMapCubit, HSSingleBoardMapState>(
-            buildWhen: (previous, current) =>
-                previous.markers != current.markers,
-            builder: (context, state) {
-              final markers = state.markers;
-              return Stack(
-                fit: StackFit.expand,
-                children: [
-                  HSGoogleMap(
-                    onMapCreated: cubit.onMapCreated,
-                    markers: markers,
-                    myLocationEnabled: true,
-                  ),
-                  Positioned(
-                    bottom: 24.0,
-                    left: builderSidePadding,
-                    right: builderSidePadding,
-                    child: SizedBox(
-                      width: builderWidth,
-                      height: 180.0,
-                      child: PageView.builder(
-                        controller: cubit.pageController,
-                        scrollDirection: Axis.horizontal,
-                        itemCount: state.spots.length,
-                        itemBuilder: (BuildContext context, int index) {
-                          return SizedBox(
-                            width: builderWidth,
-                            height: 180.0,
-                            child: HSSpotCard(
-                                spot: state.spots[index], index: index),
-                          );
-                        },
+        body: BlocSelector<HSSingleBoardMapCubit, HSSingleBoardMapState,
+            HSSingleBoardMapStatus>(
+          selector: (state) => state.status,
+          builder: (context, status) {
+            if (status == HSSingleBoardMapStatus.loading) {
+              return const HSLoadingIndicator();
+            } else if (status == HSSingleBoardMapStatus.error) {
+              return const Center(child: Text('Error loading map'));
+            }
+            return BlocBuilder<HSSingleBoardMapCubit, HSSingleBoardMapState>(
+              buildWhen: (previous, current) =>
+                  previous.markers != current.markers,
+              builder: (context, state) {
+                final markers = state.markers;
+                return Stack(
+                  fit: StackFit.expand,
+                  children: [
+                    HSGoogleMap(
+                      onMapCreated: cubit.onMapCreated,
+                      markers: markers,
+                      myLocationEnabled: true,
+                    ),
+                    Positioned(
+                      bottom: 24.0,
+                      left: builderSidePadding,
+                      right: builderSidePadding,
+                      child: SizedBox(
+                        width: builderWidth,
+                        height: 180.0,
+                        child: PageView.builder(
+                          controller: cubit.pageController,
+                          scrollDirection: Axis.horizontal,
+                          itemCount: state.spots.length,
+                          itemBuilder: (BuildContext context, int index) {
+                            return SizedBox(
+                              width: builderWidth,
+                              height: 180.0,
+                              child: HSSpotCard(
+                                  spot: state.spots[index], index: index),
+                            );
+                          },
+                        ),
                       ),
                     ),
-                  ),
-                ],
-              );
-            },
-          );
-        },
+                  ],
+                );
+              },
+            );
+          },
+        ),
       ),
     );
   }
