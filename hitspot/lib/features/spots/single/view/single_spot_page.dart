50679d9b1eb2e47ac9ac983e2dc94e1c289d720f
Wojciech Trapkowski <wojciechtrapkowski1@gmail.com>
2024-10-06 11:04:49 +0200

    Add option to choose which maps application to open

--- hitspot/lib/features/spots/single/view/single_spot_page.dart
+++ hitspot/lib/features/spots/single/view/single_spot_page.dart
@@ -24,6 +24,7 @@ import 'package:hs_authentication_repository/hs_authentication_repository.dart';
 import 'package:hs_database_repository/hs_database_repository.dart';
 import 'package:hs_location_repository/hs_location_repository.dart';
 import 'package:flutter_animate/flutter_animate.dart';
+import 'package:map_launcher/map_launcher.dart' as ml;
 
 part 'single_spot_widgets.dart';
 
@@ -209,11 +210,8 @@ class _AnimatedMapView extends StatelessWidget {
             child: ClipRRect(
               borderRadius: BorderRadius.circular(16.0),
               child: GestureDetector(
-                onTap: () => app.locationRepository.launchMaps(
-                  coords: spotLocation,
-                  description: singleSpotCubit.state.spot.getAddress,
-                  title: singleSpotCubit.state.spot.title!,
-                ),
+                onTap: () =>
+                    _showMapsChoiceBottomSheet(context, singleSpotCubit),
                 child: AbsorbPointer(
                   absorbing: true,
                   child: HSGoogleMap(
@@ -243,6 +241,47 @@ class _AnimatedMapView extends StatelessWidget {
       },
     );
   }
+}
+
+void _showMapsChoiceBottomSheet(BuildContext context, HSSingleSpotCubit cubit) {
+  final HSSingleSpotCubit singleSpotCubit = cubit;
+
+  showModalBottomSheet(
+    context: context,
+    builder: (context) => Padding(
+      padding:
+          const EdgeInsets.only(top: 8.0, bottom: 18.0, left: 8.0, right: 8.0),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          ListTile(
+              title: const Text("Open in Google Maps"),
+              onTap: () async {
+                if (await app.locationRepository.launchMaps(
+                  mapType: ml.MapType.google,
+                  coords: singleSpotCubit.spotLocation!,
+                  description: singleSpotCubit.state.spot.address!,
+                  title: singleSpotCubit.state.spot.title!,
+                )) {
+                  app.navigation.pop();
+                }
+              }),
+          ListTile(
+              title: const Text("Open in Apple Maps"),
+              onTap: () async {
+                if (await app.locationRepository.launchMaps(
+                  mapType: ml.MapType.apple,
+                  coords: singleSpotCubit.spotLocation!,
+                  description: singleSpotCubit.state.spot.address!,
+                  title: singleSpotCubit.state.spot.title!,
+                )) {
+                  app.navigation.pop();
+                }
+              }),
+        ],
+      ),
+    ),
+  );
 }
 
 class _AnimatedTagsBuilder extends StatelessWidget {
